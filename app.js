5ebb0f8b57278e20bd353733f5b935f46ecfb92a
Axel Muñoz <axelg.munoz@hotmail.com>
2025-05-06 10:49:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 const librosRoutes = require('./routes/librosRoutes')
+const usuariosRoutes = require('./routes/usuariosRoutes')
 
 app.get('/', (req, res) => {
   res.send('Alke Biblioteca')
 })
 
 app.use('/libros', librosRoutes)
+
+app.use('/usuarios', usuariosRoutes)
 
 app.listen(PORT, () => {
   console.log(`El servidor está corriendo en el puerto ${PORT}`)
