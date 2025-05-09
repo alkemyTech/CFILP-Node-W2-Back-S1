a3937aa29e9b53234aa3a1f16683997f706f90f0
Axel Muñoz <axelg.munoz@hotmail.com>
2025-05-09 18:58:20 -0300

    Usuarios with errorHandler

--- services/usuariosService.js
+++ services/usuariosService.js
@@ -1,22 +1,39 @@
 // Usuarios Service - logica de negocio
 const { Usuarios } = require ('../models')
+const { handleSequelizeError } = require("../utils/errorHandler")
 
 class UsuariosService {
 
   async getAllUsuarios () {
-    return await Usuarios.findAll()
+    try {
+      return await Usuarios.findAll()
+    } catch (error) {
+      throw handleSequelizeError(error)
+    }
   }
 
   async createUsuario(usuario) {
-    return await Usuarios.create(usuario)
+    try {
+      return await Usuarios.create(usuario)
+    } catch (error) {
+      throw handleSequelizeError(error)
+    }
   }
 
   async updateUsuario(id, usuario) {
-    return await Usuarios.update(usuario, { where: { id } })
+    try {
+      return await Usuarios.update(usuario, { where: { id } })
+    } catch (error) {
+      throw handleSequelizeError(error)
+    }
   }
 
   async deleteUsuario(id) {
-    return await Usuarios.destroy({ where: { id } })
+    try {
+      return await Usuarios.destroy({ where: { id } })
+    } catch (error) {
+      throw handleSequelizeError(error)
+    }
   }
 
 }
