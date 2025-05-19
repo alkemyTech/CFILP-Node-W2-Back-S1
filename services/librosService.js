cf7e70e6304e3217010bc4d3bab50b17e2ebe079
EzeFeito <ezepichu88@gmail.com>
2025-05-19 13:27:17 -0300

    Filtro Libros

--- services/librosService.js
+++ services/librosService.js
@@ -1,13 +1,31 @@
 // LibrosService.js - logica de negocio
+const { Op } = require('sequelize')
 const { Libros } = require('../models')
+const { handleSequelizeError } = require('../utils/errorHandler')
 
 class LibrosService {
+<<<<<<< Updated upstream
   async getAllLibros({ disponibilidad }) {
     const where = {}
     if (disponibilidad) where.disponibilidad = disponibilidad
     return await Libros.findAll({ where })
+=======
+  async getAllLibros (body) {
+    try { const where = {}
+    if(body)
+    { const { isbn, titulo, categorias, autor, anio} = body
+      if (isbn) where.isbn = { [Op.like]: `%${ isbn }%` }
+      if (titulo) where.isbn = { [Op.like]: `%${ titulo }%` }
+      if (categorias) where.isbn = { [Op.like]: `%${ categorias }%` }
+      if (autor) where.isbn = { [Op.like]: `%${ autor }%` }
+      if (anio) where.isbn = { [Op.like]: `%${ anio }%` }
+    }
+    return await Libros.findAll( {where} )
+  } catch (error) {
+    throw handleSequelizeError(error)
+    }
+>>>>>>> Stashed changes
   }
-
   async getLibroByID(id) {
     return await Libros.findOne({ where: { id } })
   }
