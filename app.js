13b60590733a004d6f1628cecb7b7573144b49da
RossoEmilia <E>
2025-05-19 17:13:22 -0300

    Swagger de libros y usuarios

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-
 const librosRoutes = require('./routes/librosRoutes')
 const usuariosRoutes = require('./routes/usuariosRoutes')
 
 app.use (express.json())
+
+const swaggerUI = require ('swagger-ui-express')
+const swaggerdocumentation = require ('./swagger.json')
+
+app.use('/documentacion', swaggerUI.serve, swaggerUI.setup(swaggerdocumentation))
 
 app.get('/', (req, res) => {
   res.send('Alke Biblioteca')
